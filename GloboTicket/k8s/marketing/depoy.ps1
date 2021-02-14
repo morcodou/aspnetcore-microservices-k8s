bf7c408590bec9de5c9bb5d1c12ad4999dc195fc
morcodou <morcodou@gmail.com>
2021-02-13 23:05:16 -0500

    add order, marketing and payment service

---
+++ GloboTicket/k8s/marketing/depoy.ps1
@@ -0,0 +1,2 @@
+kubectl create secret generic marketing-db --from-literal=connectionstring="Data Source=tcp:svc-globoticketdb,1434;Initial Catalog=GloboTicketMarketingDb;Integrated Security=False;User ID=sa;Password=P(ssw0rd0123);Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;"
+kubectl apply -f .\marketing\marketing.yaml
---
+++ GloboTicket/k8s/payment/depoy.ps1
@@ -0,0 +1,1 @@
+kubectl apply -f .\payment\payment.yaml
